cf43fbd547c09912e9942327f20ba03fa17d8c7b
SSasha88 <d1rus1853@gmail.com>
2025-01-17 18:08:27 +0300

    + Выполнено Домашнее задание по уроку "Try и Except".

--- Module_8_1.py
+++ Module_8_1.py
@@ -13,6 +13,6 @@ def add_everything_up(a, b):
         return str(a) + str(b)
 
 # Примеры использования
-print(add_everything_up(123.456, 'строка'))  # Ожидаемый вывод: 123.456строка
-print(add_everything_up('яблоко', 4215))  # Ожидаемый вывод: яблоко4215
-print(add_everything_up(123.456, 7))  # Ожидаемый вывод: 130.456
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
