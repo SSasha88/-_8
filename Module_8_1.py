eb8bb061c89285581b8cf744a624652adebe79ee
SSasha88 <d1rus1853@gmail.com>
2025-01-17 18:08:04 +0300

    + Выполнено Домашнее задание по уроку "Try и Except".

---
+++ Module_8_1.py
@@ -0,0 +1,18 @@
+print('Домашнее задание по уроку "Try и Except"')
+print()
+def add_everything_up(a, b):
+    try:
+        # Предполагаю, что a и b могут быть сложены без проблем
+        result = a + b
+
+        # Если сложение прошло успешно, возвращаю результат
+        return result
+
+    except TypeError:
+        # Если произошло исключение TypeError, значит типы a и b различны
+        return str(a) + str(b)
+
+# Примеры использования
+print(add_everything_up(123.456, 'строка'))  # Ожидаемый вывод: 123.456строка
+print(add_everything_up('яблоко', 4215))  # Ожидаемый вывод: яблоко4215
+print(add_everything_up(123.456, 7))  # Ожидаемый вывод: 130.456
